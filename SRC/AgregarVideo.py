cd13040c2d20401ea96c5aadf8d0ffe30c31224e
Sara-Osorio <62372559+Sara-Osorio@users.noreply.github.com>
2020-05-26 15:01:12 -0500

    Agregue videos

---
+++ SRC/AgregarVideo.py
@@ -0,0 +1,14 @@
+archivo = open("./archivos/Videos.txt","a",encoding='utf8')
+
+idVideos = int(input("numero del video"))
+nombre = input("nombre del video")
+url = input("enlace del video")
+fechaPublicacion = input("dia/mes/año")
+
+
+archivo.write(1 + "|" + Curso Python para Principiantes + https://www.youtube.com/watch?v=chPhlsHoEPo + 22/01/2019)
+archivo.write(2 + "|" + Lenguajes de Programación Importantes en el 2019 + https://www.youtube.com/watch?v=8u99zbGP3Lo&list=RDCMUCX9NJ471o7Wie1DQe94RVIg&index=4 + 10/01/2019)
+archivo.write(3 + "|" + Git y Github | Curso Práctico de Git y Github Desde Cero + https://www.youtube.com/watch?v=HiXLkL42tMU&list=RDCMUCX9NJ471o7Wie1DQe94RVIg&index=18 + 01/01/2018)
+
+
+archivo.close()
