6cab38063b37cbed0a06a936debc6e86edcdaa90
MAXIMILIANO <maxalvarez18@hotmail.com>
2020-05-26 19:59:47 -0500

    Agregué dos def a curso

---
+++ SRC/defCurso.py
@@ -0,0 +1,29 @@
+class Curso():
+    def __init__(self,idcurso,descripcion,idempleado):
+        self.__idcurso = idcurso
+        self.__descripcion = descripcion
+        self.__idempleado = idempleado
+    @property 
+    def idcurso(self):
+        return self.__idcurso
+    @property
+    def descripcion(self):
+        return self.__descripcion
+    @property
+    def idempleado(self):
+        return self.__idempleado   
+    def Agregar(self):
+        archivo = open("./archivos/curso.txt","a",encoding='utf8')
+        archivo.write( str(self.__idcurso) + '|' + self.__descripcion + '|' + str(self.__idempleado))
+        archivo.write("\n")    
+        archivo.close()
+    def Eliminar(self):
+        f = open("./archivos/curso.txt")
+        Lista = []
+        for line in f:
+            linea = line.split("\n")
+            self.__idcurso = linea[0]
+            self.__descripcion = linea[1]
+            self.__idempleado = linea[2]
+            if self.__idcurso != self.__idcurso:
+                Lista += line
