2bd341322c8aaa1b0502654acf4a99188f1c82da
max71120 <62411521+max71120@users.noreply.github.com>
2020-05-26 20:14:54 -0500

    Agregué def agregar y eliminar

--- SRC/clase_curso.py
+++ SRC/clase_curso.py
@@ -15,3 +15,18 @@ class cursos:
     @property
     def empleado(self)
         return self._empleado
+    def Agregar(self):
+        archivo = open("./archivos/curso.txt","a",encoding='utf8')
+        archivo.write( str(self.__idcurso) + '|' + self.__descripcion + '|' + str(self.__idempleado))
+        archivo.write("\n")    
+        archivo.close()
+    def Eliminar(self):
+        f = open("./archivos/curso.txt")
+        Lista = []
+        for line in f:
+            linea = line.split("\n")
+            self.__idcurso = linea[0]
+            self.__descripcion = linea[1]
+            self.__idempleado = linea[2]
+            if self.__idcurso != self.__idcurso:
+                Lista += line
