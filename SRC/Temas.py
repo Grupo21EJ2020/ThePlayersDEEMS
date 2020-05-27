37691ac09fb50aa40116ee631885f3fce6a52a36
max71120 <62411521+max71120@users.noreply.github.com>
2020-05-27 00:55:26 -0500

    Agregue un def

--- SRC/Temas.py
+++ SRC/Temas.py
@@ -47,3 +47,41 @@ class temas:
         archivo1.close()
         T = temas(idtema,nombre)
         T.AgregarTema()
+         def eliminarInfo(self):
+        archivo = open("./archivos/Temas.txt","r",encoding ='utf8')
+        guardTemp = []
+        for renglon in archivo:
+            datos = renglon.split("\n")
+            if datos[0] != (self.__idTema + "|" + self.__nombre):
+                guardTemp.append(datos[0])
+                archivo2 = open("./archivos/Temas.txt","w",encoding = "utf8")
+                for i in guardTemp:
+                    archivo2.write(i + "\n")
+                archivo2.close()
+        guardTemp.remove()
+        archivo.close()
+        
+        def cambiarInfo(self):
+        lista = []
+        cam = []
+        archivo = open("./archivos/Temas.txt","r",encoding ='utf8')
+        guardado = archivo.readlines()
+        for gard in guardado:
+            gard = gard.replace("\n", "")
+            lista.append(gard)
+        for renglon in lista:
+            datos = renglon.split("|")
+            if datos[0] == (self.__idTema):
+                datosNuevos = datos[1].replace(datos[1], self.__nombre + "\n")
+                datosCambiados = (datos[0] + "|" + datosNuevos)
+                cam.append(datosCambiados)
+            else:
+                datos = (renglon + "\n")
+                cam.append(datos)
+        archivos2 = open("./archivos/Temas.txt","w",encoding = "utf8")                
+        for c in cam:
+            cambios = c
+            archivos2.write(cambios)       
+        archivos2.close()
+        
+        archivo.close()
