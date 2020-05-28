abf9969a6e5b84dbf42fc06f0a67e50ff3177482
Sara-Osorio <62372559+Sara-Osorio@users.noreply.github.com>
2020-05-27 19:26:27 -0500

    Add Ver informacion de videos

---
+++ SRC/VerInfoVideos.py
@@ -0,0 +1,11 @@
+# Ver informacion de videos
+
+def  info_de_videos ():
+        archivo  =  open( "./archivos/videos.txt" ,'r',encoding= "utf8" )
+
+        print ( "Ingrese el id de video: ")
+        id_buscar  =  input ( ">" )
+
+        
+
+archivo.close()
