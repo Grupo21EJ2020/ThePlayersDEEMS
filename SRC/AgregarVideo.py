ab36750a7b49fe0ee5a6b34f1b108e3b51732422
Sara-Osorio <62372559+Sara-Osorio@users.noreply.github.com>
2020-05-27 20:18:48 -0500

    Agruege Videos

--- SRC/AgregarVideo.py
+++ SRC/AgregarVideo.py
@@ -1,10 +1,10 @@
 archivo = open("./archivos/videos.txt","a",encoding='utf8')
 
 
-archivo.write(1 + "|" + Curso Python para Principiantes + https://www.youtube.com/watch?v=chPhlsHoEPo + 22/01/2019)
-archivo.write(2 + "|" + Lenguajes de Programación Importantes en el 2019 + https://www.youtube.com/watch?v=8u99zbGP3Lo&list=RDCMUCX9NJ471o7Wie1DQe94RVIg&index=4 + 10/01/2019)
-archivo.write(3 + "|" + Git y Github | Curso Práctico de Git y Github Desde Cero + https://www.youtube.com/watch?v=HiXLkL42tMU&list=RDCMUCX9NJ471o7Wie1DQe94RVIg&index=18 + 01/01/2018)
-
+archivo.write(1 + "|" + Curso Python para Principiantes + "|" + https://www.youtube.com/watch?v=chPhlsHoEPo + "|" + 22/01/2019)
+archivo.write(2 + "|" + Lenguajes de Programación Importantes en el 2019 + "|" + https://www.youtube.com/watch?v=8u99zbGP3Lo&list=RDCMUCX9NJ471o7Wie1DQe94RVIg&index=4 + 10/01/2019)
+archivo.write(3 + "|" + Git y Github | Curso Práctico de Git y Github Desde Cero + "|" + https://www.youtube.com/watch?v=HiXLkL42tMU&list=RDCMUCX9NJ471o7Wie1DQe94RVIg&index=18 + 01/01/2018)
+archivo.write(4 + "|" + Curso Python. Sintaxis Básica III Funciones I. Vídeo 5 + "|" + https://www.youtube.com/watch?v=VY448UWAQ_0 + "|" + 20/02/2017)
 
 archivo.close()
 
